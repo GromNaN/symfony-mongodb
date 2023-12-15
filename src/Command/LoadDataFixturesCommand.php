0f4acf82ddf288d6706c6df234885b62173154f1
Jérôme Tamarelle <jerome@tamarelle.net>
2023-12-15 11:31:06 +0100

    Add fixtures mecanism

---
+++ src/Command/LoadDataFixturesCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2023-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace MongoDB\Bundle\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ServiceLocator;
+
+#[AsCommand(
+    name: 'mongodb:fixtures:load',
+    description: 'Load data fixtures to your database.',
+)]
+final class LoadDataFixturesCommand extends Command
+{
+    private SymfonyStyle $io;
+
+    public function __construct(
+        private readonly ServiceLocator $clients,
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->io = new SymfonyStyle($input, $output);
+
+        $this->printExtensionInformation();
+        $this->printClientInformation();
+
+        // TODO: Add some information about the used attributes and their configs!
+
+        return Command::SUCCESS;
+    }
+}
